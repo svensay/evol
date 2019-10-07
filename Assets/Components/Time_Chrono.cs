397e9729b49ea4de28a1c57eec59f2414fbf9b4e
svensay <svensay@gmail.com>
2019-10-07 15:09:53 +0200

    Ajout du time

---
+++ Assets/Components/Time_Chrono.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Time_Chrono : MonoBehaviour {
+    // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+    public Text affichage;
+}
---
+++ Assets/Systems/TimeSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using FYFY;
+
+public class TimeSystem : FSystem {
+
+    private Family _TimeFamily = FamilyManager.getFamily(new AllOfComponents(typeof(Time_Chrono)));
+    private float chronoTime;
+
+    // Use to process your families.
+    protected override void onProcess(int familiesUpdateCount)
+    {
+        foreach (GameObject go in _TimeFamily)
+        {
+            Time_Chrono c = go.GetComponent<Time_Chrono>();
+            chronoTime += Time.deltaTime;
+            c.affichage.text = chronoTime.ToString();
+        }
+    }
+}
