0dbee59c6bb4b8fcabc44aa4eaf6649620acd7e3
svensay <svensay@gmail.com>
2019-10-07 15:43:04 +0200

    Attribut Oiseau

---
+++ Assets/Components/Attribut.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Attribut : MonoBehaviour {
+    // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+    public string nom;
+    public Text nom_aff;
+}
---
+++ Assets/Systems/AffichageAttributSystem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using FYFY;
+
+public class AffichageAttributSystem : FSystem {
+
+    private Family _AffichageAttribut = FamilyManager.getFamily(new AllOfComponents(typeof(Attribut)));
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+       /** foreach(GameObject go in _AffichageAttribut)
+        {
+            Attribut a = go.GetComponent<Attribut>();
+            a.nom_aff.text = a.nom;
+        }
+    */
+    }
+    public void OnMouseDown()
+    {
+        foreach (GameObject go in _AffichageAttribut)
+        {
+            Attribut a = go.GetComponent<Attribut>();
+            a.nom_aff.text = a.nom;
+            Debug.Log("OnMouse");
+        }
+    }
+}
