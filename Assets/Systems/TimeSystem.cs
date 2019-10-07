a318770425a8963f112f167d9bcd5aca563ce32e
svensay <svensay@gmail.com>
2019-10-07 17:37:47 +0200

    Je ne sais pas

--- Assets/Systems/TimeSystem.cs
+++ Assets/Systems/TimeSystem.cs
@@ -4,6 +4,7 @@ using FYFY;
 public class TimeSystem : FSystem {
 
     private Family _TimeFamily = FamilyManager.getFamily(new AllOfComponents(typeof(Time_Chrono)));
+    private float chronoTime;
 
     // Use to process your families.
     protected override void onProcess(int familiesUpdateCount)
@@ -11,9 +12,12 @@ public class TimeSystem : FSystem {
         foreach (GameObject go in _TimeFamily)
         {
             Time_Chrono c = go.GetComponent<Time_Chrono>();
-            float chronoTime = float.Parse(c.affichage.text);
+            
             chronoTime += Time.deltaTime;
-            c.affichage.text = chronoTime.ToString();
+            int temp = (int)chronoTime;
+            int min = temp / 60;
+            int sec = temp % 60;
+            c.affichage.text = min.ToString() + ":" + sec.ToString();
         }
     }
 }
